40bbdb88624abd3fe28302913117a5fc6b019bcd
aniketsalgaonkar <aniketsalgaonkar.nit@gmail.com>
2025-06-18 23:40:02 +0530

    add templates again

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,6 @@
-// import template1 from './template1.png';
-// import template2 from './template2.png';
-// import template3 from './template3.png';
+import template1 from "./template1.png";
+import template2 from "./template2.png";
+import template3 from "./template3.png";
 import template4 from "./template4.png";
 import template5 from "./template5.png";
 import upload_area from "./upload_area.png";
@@ -11,9 +11,9 @@ import landing3 from "./landing3.png";
 import landing4 from "./landing4.png";
 
 export const assets = {
-  // template1,
-  // template2,
-  // template3,
+  template1,
+  template2,
+  template3,
   template4,
   template5,
   upload_area,
@@ -25,9 +25,9 @@ export const assets = {
 };
 
 export const templates = [
-  // { id: "template1", label: "Template 1", image: assets.template1 },
-  // { id: "template2", label: "Template 2", image: assets.template2 },
-  // { id: "template3", label: "Template 3", image: assets.template3 },
-  { id: "template4", label: "Template 1", image: assets.template4 },
-  { id: "template5", label: "Template 2", image: assets.template5 },
+  { id: "template1", label: "Template 1", image: assets.template1 },
+  { id: "template2", label: "Template 2", image: assets.template2 },
+  { id: "template3", label: "Template 3", image: assets.template3 },
+  { id: "template4", label: "Template 4", image: assets.template4 },
+  { id: "template5", label: "Template 5", image: assets.template5 },
 ];
